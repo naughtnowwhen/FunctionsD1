4aedbd5a6c5868555edfa4ede5c3ee81196ff178
Peter <naughtnowwhen@gmail.com>
2018-09-15 16:35:54 -0700

    wrappping in functions

--- main.js
+++ main.js
@@ -1,118 +1,190 @@
 'use strict';
 
 //Welcome message
-var userName = prompt('Hi, welcome to my About Nikki site, what\s your name?');
-alert('Welcome to my page ' + userName +'!');
-console.log('welcome to my page ' + userName);
+// var userName = prompt('Hi, welcome to my About Nikki site, what\s your name?');
+// alert('Welcome to my page ' + userName +'!');
+
+var rightAnswers = [];
+var wrongAnswers = [];
+
+
+
 
 //Bulldog question
-var bullDog = prompt('Does Nikki want a French Bulldog?');
-bullDog = bullDog.toLowerCase();
-console.log('Does Nikki want a French Bulldog?' + bullDog);
+var functionBulldog = function(){
+  var bullDog = prompt('Does Nikki want a French Bulldog?');
+  bullDog = bullDog.toLowerCase();
+//   bullDog = bullDog.toLowerCase();
 
-if(bullDog === 'yes' || bullDog === 'y') {
+  if(bullDog === 'yes' || bullDog === 'y') {
+    rightAnswers.push(bullDog);
     alert('Way to go! I really want a french bulldog!');
-}
+  }
 
-else if (bullDog === 'no' || bullDog === 'n') {
-    alert('Oh no! I love frenchies!')
-}
+  else if (bullDog === 'no' || bullDog === 'n') {
+    wrongAnswers.push(bullDog);
+    alert('Oh no! I love frenchies!');
+  }
 
-else {
-    alert('Please respond using y or n, or yes or no.');
-}
+  else {
+    var bullDog = prompt('Please respond using y or n, or yes or no.');
+  }
+};
+
+functionBulldog();
+
+
 
 //Pets Question
-var pets = prompt('Does Nikki have any pets?');
-pets = pets.toLowerCase();
-console.log('Does Nikki have any pets?' + pets);
 
-if(pets === 'yes' || pets === 'y') {
+var functionPets = function(){
+  var pets = prompt('Does Nikki have any pets?');
+  pets = pets.toLowerCase();
+
+  if(pets === 'yes' || pets === 'y') {
     alert('You are correct! She as 2 cats: Waldo and Nala!');
-}
+    rightAnswers.push(pets);
+  }
 
-else if(pets ==='no' || pets === 'n') {
+  else if(pets ==='no' || pets === 'n') {
     alert('You are incorrect, minus 5 points for Slytherin!');
-}
+    wrongAnswers.push(pets);
+  }
 
-else {
-    alert('Please respond using y or n, or yes or no.');
-}
+  else {
+    var pets = prompt('Please respond using y or n, or yes or no.');
+  }
+
+};
+
+functionPets();
+
+
 
 //Snowman question
-var snowman = prompt('Does Nikki want to build a snowman?');
-snowman = snowman.toLowerCase();
-console.log('Does Nikki want to build a snowman?' + snowman)
 
-if(snowman === 'yes' || snowman === 'y') {
+var functionSnowman = function(){
+
+  var snowman = prompt('Does Nikki want to build a snowman?');
+  snowman = snowman.toLowerCase();
+  console.log('Does Nikki want to build a snowman?' + snowman);
+
+  if(snowman === 'yes' || snowman === 'y') {
     alert('I always want to build a snowman!');
-}
+    rightAnswers.push(snowman);
 
-else if (snowman === 'no' || snowman === 'n') {
+  }
+
+  else if (snowman === 'no' || snowman === 'n') {
     alert('Who doesn\'t want to build a snowman?');
-}
+    wrongAnswers.push(snowman);
 
-else {
-    alert('Please respond using y or n, or yes or no.');
-}
+  }
 
+  else {
+    var snowman = prompt('Please respond using y or n, or yes or no.');
+  }
+};
+
+functionSnowman();
+
+
+var functionMangoes = function() {
 //Mangoes question
-var mangoes = prompt('Does Nikki love mangoes?');
-mangoes = mangoes.toLowerCase();
-console.log('Does Nikki love mangoes?' + mangoes);
+  var mangoes = prompt('Does Nikki love mangoes?');
+  mangoes = mangoes.toLowerCase();
 
-if(mangoes === 'yes' || mangoes === 'y') {
+  if(mangoes === 'yes' || mangoes === 'y') {
     alert('I looooove mangoes!');
-}
+    rightAnswers.push(mangoes);
+  }
 
-else if (mangoes === 'NO' || mangoes === 'N') {
+  else if (mangoes === 'NO' || mangoes === 'N') {
     alert('Mangoes are life!');
-}
+    wrongAnswers.push(mangoes);
+  }
 
-else {
-    alert('Please respond using y or n, or yes or no.');
-}
+  else {
+    var mangoes = prompt('Please respond using y or n, or yes or no.');
+  }
 
- //FairyTail question
-var fairyTail = prompt('Is Nikki\'s favorite anime FairyTail?');
-fairyTail = fairyTail.toLowerCase();
-console.log('Is Nikki\'s favorite anime FairyTail?' + fairyTail);
+};
+functionMangoes();
 
-if(fairyTail === 'yes' || fairyTail === 'y') {
+
+
+
+//FairyTail question
+
+var functionFairyTail = function() {
+
+  var fairyTail = prompt('Is Nikki\'s favorite anime FairyTail?');
+  fairyTail = fairyTail.toLowerCase();
+
+  if(fairyTail === 'yes' || fairyTail === 'y') {
+    rightAnswers.push(fairyTail);
     alert('Who is your favorite character?');
-}
+  }
 
-else if (fairyTail === 'no' || fairyTail === 'n') {
+  else if (fairyTail === 'no' || fairyTail === 'n') {
+    wrongAnswers.push(fairyTail);
     alert('You are the weakest link! Goodbye!');
-}
-else {
-    alert('Please respond using y or n, or yes or no.');
-}
+  }
+  else {
+    var fairyTail = prompt('Please respond using y or n, or yes or no.');
+  }
+};
 
-//Guess number
-var guess= prompt('Can you guess my favorite number? It is between 1-20.');
-console.log('Can you guess my favorite number?' + guess);
+functionFairyTail();
 
-var number = 16;
-var guesses = parseInt(guess);
-for(var i = 0; i < 4; i++) { 
- if(guesses === number){
-    alert('Homerun! You guessed my number!');
-    console.log('homerun' + guesses)
-    break;
- } else if(guesses < number) {
-     guess = prompt('Your guess was too low. There are no low riders here so please try again!');
-     guesses = parseInt(guess);
-     console.log('too low' + guess);
-} else (guesses > number) 
-    guess = prompt('Whoa! That\'s too high. Guess again.');
-    guesses = parseInt(guess);
-    console.log('Whoa! That\'s too high.' + number);
-}
 
-//multiple answers
-//global variables
-var answeredCorrectly;
+
+
+
+Guess number
+
+
+
+
+var functionNumberGame = function () {
+  var guess= prompt('Can you guess my favorite number? It is between 1-20.');
+  var guessCorrect = false;
+  var turnsLeft = 4;
+  do {
+    var number = 16;
+    var guesses = parseInt(guess);
+    for(var i = 0; i < 4; i++) {
+    //   turnsLeft --;
+      console.log(turnsLeft, 'this is turns left is it running?');
+    }
+    if(guesses === number){
+      alert('Homerun! You guessed my number!');
+      console.log('homerun' + guesses);
+      guessCorrect = true;
+      rightAnswers.push();
+      break;
+      console.log(guessCorrect);
+    } else if(guesses < number) {
+      guess = prompt('Your guess was too low. There are no low riders here so please try again!');
+      console.log(turnsLeft);
+      turnsLeft --;
+      guesses = parseInt(guess);
+      console.log('too low' + guess);
+    } else if(guesses > number){
+      guess = prompt('Whoa! That\'s too high. Guess again.');
+      guesses = parseInt(guess);
+      console.log('Whoa! That\'s too high.' + number);
+    }
+    // guessCorrect === false && turnsLeft >0
+  } while(guessCorrect === false && turnsLeft >0);
+};
+
+functionNumberGame();
+
+
+// multiple answers
+// global variables
+var answeredCorrectly = false;
 var correctGuess = 0;
 var guessCounter = 6;
 var countries = ['Germany', 'Czech Republic', 'Poland', 'Lithuania', 'Japan'];
@@ -135,4 +207,4 @@ if(answeredCorrectly) {
     alert('Nice try but guess again.');
 }
     guessCounter--;
-} while(guessedCounter > 0 && !answeredCorrectly);
+} while(guessedCounter > 0 && answeredCorrectly);
---
+++ other.js
@@ -0,0 +1,2 @@
+// console.log('welcome to my page ' + userName);
+// console.log('Does Nikki want a French Bulldog?' + bullDog);
